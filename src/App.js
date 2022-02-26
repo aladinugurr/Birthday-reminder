9fa0d3bc3178ebc9f3fac8d58a18efffe6d0579e
Aladin <ugur.catal8@gmail.com>
2022-02-26 13:02:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Users from './Users'
+import data from './Data'
+
 
 function App() {
+  const [people,setPeople] = useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+    <div className="container ">
+    <h3>{people.length} birthdays today</h3>
+
+     <Users people={people} />
+     <button onClick={()=>{setPeople([])}}>Clear All</button>
     </div>
+    </section>    
   );
 }
 
---
+++ src/Users.js
@@ -0,0 +1,26 @@
+import {React} from 'react'
+
+
+function Users({people}) {
+    
+  return (
+     <>
+    {people.map((person)=>{
+        const {id,name,age,image} =  person;
+        return(
+         <article key={id} className="person">
+         <img src={image} alt={name} />
+         <div>
+             <h4>{name}</h4>
+             <p>{age} years</p>
+         </div>
+
+         </article >
+         );
+    })}
+    </>
+  );
+  
+}
+
+export default Users
